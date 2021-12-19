b2ff5027e30c3e086f77e76439a039b2ab7e4a9e
khazaddoom <31121986g@gmail.com>
2021-12-19 09:01:52 +0530

    2 files modified

--- src/App.js
+++ src/App.js
@@ -2,5 +2,5 @@ import React from 'react';
 import './style.css';
 
 export default function App() {
-  return <h1>Hello</h1>;
+  return <h1>Hello asdasd</h1>;
 }
--- src/withToggle.js
+++ src/withToggle.js
@@ -1,7 +1,8 @@
 import React from 'react';
 
 export default function withToggle(Component) {
-  return function WithToggle() {
-    return <Component />;
+  return function WithToggle(props) {
+    const [toggle, setToggle] = React.useState(false);
+    return <Component toggle={toggle} setToggle={setToggle} {...props} />;
   };
 }
